8ab8192310381c6627d0a08a3cf9849b98b2cb2b
AdrielCMSS <159402468+AdrielCMSS@users.noreply.github.com>
2024-02-08 22:49:56 -0300

    Add - Correção de registro
    Corrigido bug que reescrevia sobre registro já efetuado de cpf

--- ProjetoCartorioEBAC_AdrielCesar.c
+++ ProjetoCartorioEBAC_AdrielCesar.c
@@ -45,7 +45,6 @@ int erronome()
 
 int registrar() //Funcao responsavel por cadastras os usuários no sistema
 {
-
 	setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
 			
 	//início da criação de variaveis / string
@@ -61,45 +60,57 @@ int registrar() //Funcao responsavel por cadastras os usuários no sistema
 	scanf("%s", cpf);// %s refere-se a string
 	
 	strcpy(arquivo, cpf); //responsável por copiar os valores das string
-	
-	
+
+
 	FILE *file; //cria o arquivo
-	file = fopen(arquivo,"w"); // W significa cria o arquivo
-	fprintf(file,"CPF: ");
-	fprintf(file,cpf); //salvo o valor da variavel
-	fprintf(file,"\n");
-	fclose(file); //fecha o arquivo
+	file = fopen(cpf,"r");
 	
+	if(file != NULL)
+	{
+		printf("Esse CPF já existe no sistema!\n");
+		fclose(file);
+	}
 	
-	printf("Digite o NOME a ser cadastrado:");
-	scanf("%s",nome);
+	else
+	{
+		fclose(file);
 	
-	file = fopen(arquivo, "a");//A significa alterar arquivo
-	fprintf(file,"Nome: ");
-	fprintf(file,nome);
-	fprintf(file,"\n");
-	fclose(file);
+		file = fopen(arquivo,"w"); // W significa cria o arquivo
+		fprintf(file,"CPF: ");
+		fprintf(file,cpf); //salvo o valor da variavel
+		fprintf(file,"\n");
+		fclose(file); //fecha o arquivo
+
+
+		printf("Digite o NOME a ser cadastrado:");
+		scanf("%s",nome);
+	
+		file = fopen(arquivo, "a");//A significa alterar arquivo
+		fprintf(file,"Nome: ");
+		fprintf(file,nome);
+		fprintf(file,"\n");
+		fclose(file);
 	
 		
-	printf("Digite o SOBRENOME a ser cadastrado:");
-	scanf("%s",sobrenome);
+		printf("Digite o SOBRENOME a ser cadastrado:");
+		scanf("%s",sobrenome);
 	
-	file = fopen(arquivo, "a");
-	fprintf(file,"Sobrenome: ");
-	fprintf(file,sobrenome);
-	fprintf(file,"\n");
-	fclose(file);
+		file = fopen(arquivo, "a");
+		fprintf(file,"Sobrenome: ");
+		fprintf(file,sobrenome);
+		fprintf(file,"\n");
+		fclose(file);
 	
 	
-	printf("Digite o CARGO a ser cadastrado:");
-	scanf("%s",cargo);
-	
-	file = fopen(arquivo, "a");
-	fprintf(file,"Cargo: ");
-	fprintf(file,cargo);
-	fprintf(file,"\n");
-	fclose(file);
+		printf("Digite o CARGO a ser cadastrado:");
+		scanf("%s",cargo);
 
+		file = fopen(arquivo, "a");
+		fprintf(file,"Cargo: ");
+		fprintf(file,cargo);
+		fprintf(file,"\n");
+		fclose(file);	
+	}	
 	voltar1();
 }
 
@@ -158,7 +169,6 @@ int deletar()
 	{
 		printf("%s", conteudo);
 	}
-
 	fclose(file);
 
 	remove(cpf);
@@ -188,11 +198,9 @@ int voltar1()
 		break;
 		
 		default:
-		system("cls");
-		voltar1()
+		voltar1();
 		break;
 	}
-
 }
 
 int voltar2()
@@ -217,11 +225,9 @@ int voltar2()
 		break;
 		
 		default:
-		system("cls");
-		voltar2()
+		voltar2();
 		break;
 	}
-
 }
 
 int voltar3()
@@ -246,71 +252,64 @@ int voltar3()
 		break;
 		
 		default:
-		system("cls");
-		voltar3()
+		voltar3();
 		break;
 	}
-
 }
 
 int main()
 {
 	int opcao; //Definindo variáveis
-
+	
+	system("cls");//Limpar a tela
+		
+	setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+		
+		//Início do Menu Inicial
+		
+	cabecalho();
+	
+		//Opções
+	printf(" ESCOLHA UMA DAS OPÇÕES ABAIXO: \n");
+	printf("\t 1 - Registrar Nomes \n");
+	printf("\t 2 - Consultar Nomes \n");
+	printf("\t 3 - Deletar Nomes \n");
+	printf("\t 4 - Fechar o sistema \n");
+		//Fim do Menu Inicial
+			
+	printf("Opcão: ");
+	scanf("%d", &opcao);//Armazenando a escolha do usuário
+	
+			
+	system("cls");//Limpar a tela
+	
+		//Inicío da seleção
+	switch(opcao)
 	{
-		system("cls");//Limpar a tela
-		
-		setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
-		
-			//Início do Menu Inicial
-		
+		case 1:
 		cabecalho();
-	
-			//Opções
-		printf(" ESCOLHA UMA DAS OPÇÕES ABAIXO: \n");
-		printf("\t 1 - Registrar Nomes \n");
-		printf("\t 2 - Consultar Nomes \n");
-		printf("\t 3 - Deletar Nomes \n");
-		printf("\t 4 - Fechar o sistema \n");
-			//Fim do Menu Inicial
+		registrar();
+		break;
 			
-		printf("Opcão: ");
-		scanf("%d", &opcao);//Armazenando a escolha do usuário
-	
+		case 2:
+		cabecalho();
+		consultar();
+		break;
 			
-		system("cls");//Limpar a tela
-	
-			//Inicío da seleção
-		switch(opcao)
-		{
-			case 1:
-			cabecalho();
-			registrar();
-			break;
+		case 3:
+		cabecalho();
+		deletar();
+		break;
 			
-			case 2:
-			cabecalho();
-			consultar();
-			break;
-			
-			case 3:
-			cabecalho();
-			deletar();
-			break;
-			
-			case 4:
-			cabecalho();
-			printf("<<<Obrigado por utilizar o sistema>>> \n\n");
-			return 0;
-			break;
+		case 4:
+		cabecalho();
+		printf("<<<Obrigado por utilizar o sistema>>> \n\n");
+		return 0;
+		break;
 						
-			default:
-			erro();
-			main();
-			
-			break;
-			
-		}
-			//Fim da seleção
-	}
+		default:
+		erro();
+		main();
+		break;
+	}	//Fim da seleção			
 }
